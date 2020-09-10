f5a9c9baf6db77b188798355c2254d4ca2f77e5b
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-09-10 18:53:17 +0200

    100% union coverage

---
+++ test/handlers/union/optional/index.spec.ts
@@ -0,0 +1,20 @@
+import fixture from "./fixture";
+import { expect } from "chai";
+import yxc, { createExecutableSchema } from "../../../../src/index";
+
+describe("Union optional", () => {
+  for (const test of fixture) {
+    const handler = yxc.union([yxc.string(), yxc.number()]).optional();
+    it(`${test.value} should be a ${
+      test.expected ? "valid" : "invalid"
+    } optional string-number union`, () => {
+      const result = createExecutableSchema(handler)(test.value);
+      if (test.expected) {
+        expect(result.errors).to.be.empty;
+      } else {
+        expect(result.errors).to.not.be.empty;
+      }
+      expect(result.ok).to.be.equal(test.expected);
+    });
+  }
+});
